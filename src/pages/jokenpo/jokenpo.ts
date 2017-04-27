1b4b884c05d4d270cefd95122de712fc50ce71db
Jorge Junior <jorgejunior@guimo.toys>
2017-04-27 10:50:05 -0300

    Prepared jokenpo to send bluetooth string

--- src/pages/jokenpo/jokenpo.ts
+++ src/pages/jokenpo/jokenpo.ts
@@ -1,3 +1,5 @@
+import { Guimo } from './../../providers/guimo';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
@@ -24,7 +26,10 @@ export class JokenpoPage {
   ]
   result:string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private blt: BluetoothSerial) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JokenpoPage');
@@ -43,50 +48,58 @@ export class JokenpoPage {
       if(playerChoose == 1 && (this.guimoChoose == 3 )){
         this.myPoints++;
         this.result = this.msgs[0];
-        console.log(this.result);
+       // this.blt.write("tesoura\n");
       }
 
       //Se o player escolher Papel e o Guimo Pedra, PlayerGanha
       if(playerChoose == 2 && (this.guimoChoose == 1)){
         this.myPoints++;
         this.result = this.msgs[0];
+        // this.blt.write("pedra\n");
       }
 
       //Se o player escolher Tesoura e o Guimo Papel, PlayerGanha
       if(playerChoose == 3 && (this.guimoChoose == 2)){
         this.myPoints++;
         this.result = this.msgs[0];
+        // this.blt.write("papel\n");
       }
 
       //Se o player escolher Pedra e o Guimo Papel, Guimo Ganha
       if(playerChoose == 1 && (this.guimoChoose == 2)){
         this.guimoPoints++;
         this.result = this.msgs[1];
+        // this.blt.write("papel\n");
       }
 
       //Se o player escolher Papel e o Guimo Tesoura, Guimo Ganha
       if(playerChoose == 2 && (this.guimoChoose == 3)){
         this.guimoPoints++;
         this.result = this.msgs[1];
+        // this.blt.write("tesoura\n");
       }
 
       //Se o player escolher Tesoura e o Guimo Pedra, Guimo
       if(playerChoose == 3 && (this.guimoChoose == 1)){
         this.guimoPoints++;
         this.result = this.msgs[1];
+        // this.blt.write("pedra\n");
       }
 
       if(playerChoose == this.guimoChoose){
         this.guimoPoints++;
         this.myPoints++;
         this.result = this.msgs[2];
+        // this.blt.write("empate\n");
       }
+      
       this.guimoPlayed = true;
       setTimeout(()=>{
         this.playing = false;
         this.guimoChoose = 0;
         this.guimoPlayed = false;
         this.result = "";
+        this.blt.write(Guimo.SCREEN_DEFAULT);
        },5000);
 
     }
