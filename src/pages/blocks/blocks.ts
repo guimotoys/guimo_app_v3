d7a48c753693e881b6c9c0fc6df1b7479afe3254
Jorge David <jorgejunior@guimo.toys>
2017-07-31 20:55:04 -0300

    Created cleanArrayCode to clean spaces in array of codes

--- src/pages/blocks/blocks.ts
+++ src/pages/blocks/blocks.ts
@@ -17,14 +17,14 @@ declare var Blockly: any;
 export class BlocksPage {
   workspace: any;
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     private screenOrientation: ScreenOrientation,
     private blt: BluetoothSerial,
-    private guimo: Guimo) {}
+    private guimo: Guimo) { }
 
-  ionViewDidLoad(){
-    this.workspace = Blockly.inject('blockly',{
+  ionViewDidLoad() {
+    this.workspace = Blockly.inject('blockly', {
       toolbox: document.getElementById('toolbox'),
       zoom: {
         controls: false,
@@ -33,83 +33,115 @@ export class BlocksPage {
         maxScale: 3,
         minScale: 0.3
       },
-     trashcan: false
+      trashcan: false
     });
 
   };
 
-  ionViewWillLoad(){
-    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE).then(()=>{
-      console.log('Orientation Locked in '+this.screenOrientation.ORIENTATIONS.LANDSCAPE);
-    }).catch( err =>{
+  ionViewWillLoad() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE).then(() => {
+      console.log('Orientation Locked in ' + this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+    }).catch(err => {
       console.log(err);
     });
   }
 
-  ionViewWillLeave(){
-    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT).then( () =>{
-      console.log('Screen Orientation '+this.screenOrientation.ORIENTATIONS.PORTRAIT);
+  ionViewWillLeave() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT).then(() => {
+      console.log('Screen Orientation ' + this.screenOrientation.ORIENTATIONS.PORTRAIT);
     });
-  } 
+  }
 
-  runCode(){
+  backToMenu() {
+    console.log('entrou backToMenu');
+    this.navCtrl.pop();
+  }
   
+  
+  runCode() {
+
     Blockly.JavaScript.addReservedWords('code');
     var code = Blockly.JavaScript.workspaceToCode(this.workspace);
     let codes = code.split(',');
     
+    codes = this.cleanArrayCode(codes);
     console.log(codes);
-    let i =0;
-    let tam = codes.length -1;
+
+    let codIf = codes[0].split(':');
+    if (codIf[0] == 'cond') {
+        console.log(codIf);
+    } else {
+      //this.runCodeWithoutIf(codes);
+    }
+  }
+
+  /**
+   *  Run blockly codes when if is out/not present
+   * @param codes Array With Codes by Blockly
+   */
+  private runCodeWithoutIf(codes: any) {
+    console.log(codes);
+    let i = 0;
+    let tam = codes.length - 1;
     let repeatQtd = parseInt(codes[tam]);
 
-    /*if(!isNaN(repeatQtd)){
-        var codesLength = codes.length - 1;
-        var code;
-        var arrayCodes: Array<any> = [];
-        var arrayCodeRun: Array<any> = [];
-        while(i < codesLength){
-          code = codes[i].split("  ");
-          for(var j = 0; j < code.length; j++){
-            if(code[j] != ""){
-              arrayCodes.push(code[j]);
-            }
-          }
-          i++;
-        }
-        
-        for(var k = 0; k < repeatQtd; k++){
-          for(var m = 0; m < arrayCodes.length; m++){
-              arrayCodeRun.push(arrayCodes[m]);
+    /*if (!isNaN(repeatQtd)) {
+      var codesLength = codes.length - 1;
+      var code;
+      var arrayCodes: Array<any> = [];
+      var arrayCodeRun: Array<any> = [];
+      while (i < codesLength) {
+        code = codes[i].split("  ");
+        for (var j = 0; j < code.length; j++) {
+          if (code[j] != "") {
+            arrayCodes.push(code[j]);
           }
         }
-        
-
-        for(var n = 0; n < arrayCodeRun.length; n++){
-          this.runCodeSleepLoop(n,arrayCodeRun[n],500);
-        };
+        i++;
       }
 
-      /*if(isNaN(repeatQtd)){
-        for(var n = 0; n < codes.length; n++){
-          this.runCodeSleepLoop(n, codes[n],500);
+      for (var k = 0; k < repeatQtd; k++) {
+        for (var m = 0; m < arrayCodes.length; m++) {
+          arrayCodeRun.push(arrayCodes[m]);
         }
-      }*/
+      }
+
+
+      for (var n = 0; n < arrayCodeRun.length; n++) {
+        this.runCodeSleepLoop(n, arrayCodeRun[n], 500);
+      };
+    }
+
+    if (isNaN(repeatQtd)) {
+      for (var n = 0; n < codes.length; n++) {
+        this.runCodeSleepLoop(n, codes[n], 500);
+      }
+    }*/
   }
 
-  private runCodeSleepLoop(i:number, code:any,time:number){
-    setTimeout(()=>{
+  private runCodeSleepLoop(i: number, code: any, time: number) {
+    setTimeout(() => {
       //console.log(code);
       //SEND BLUETOOTH CODE HERE
-      this.blt.write(code).then(()=>{
-        console.log('Código '+code+ ' enviado');
+      this.blt.write(code).then(() => {
+        console.log('Código ' + code + ' enviado');
       });
-    },(i+1)*time);
+    }, (i + 1) * time);
   }
 
-  backToMenu(){
-    console.log('entrou backToMenu');
-    this.navCtrl.pop();
+  /**
+   * clean any space in codes
+   * @param codes array of Blockly Codes
+   */
+  private cleanArrayCode(codes:any){
+    for (var i = 0; i < codes.length; i++) {
+      /**Remove all spaces from vector / /g means all global spaces */
+      codes[i] = codes[i].replace(/ /g, "");
+      if(codes[i] == ""){
+        codes.splice(i,1);
+      }
+      
+    }
+    return codes;
   }
-
 }
